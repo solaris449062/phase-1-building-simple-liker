a1f8e6be70c26b07df28114632882447c68871ac
Sungoh Park <spark449062@gmail.com>
2021-07-30 01:34:51 -0600

    commit tree 44263bf781a0c10c0bc14f69c0f1e1c1f95828ac

--- main.js
+++ main.js
@@ -3,9 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorDivElement = document.querySelector("div#modal");
+const errorMessageElement = document.querySelector("p#modal-message");
+errorMessageElement.innerText = "Random server error. Try again."
+errorDivElement.setAttribute("class", "hidden");
+let isLiked = false;
 
-
-
+let heartElementArray = document.querySelectorAll("span.like-glyph")
+for (let i = 0; i < heartElementArray.length; i++) {
+  heartElementArray[i].addEventListener("click", function() {
+    mimicServerCall()
+    .then(() => {
+      errorDivElement.setAttribute("class", "hidden");
+      isLiked = !isLiked;
+      if (isLiked) {
+        heartElementArray[i].innerText = FULL_HEART;
+        heartElementArray[i].setAttribute("class", "activated-heart");
+      }
+      else {
+        heartElementArray[i].innerText = EMPTY_HEART;
+        heartElementArray[i].removeAttribute("class");
+      }
+    })
+    .catch(() => {
+      errorDivElement.removeAttribute("class");
+      setTimeout(function() {
+        errorDivElement.setAttribute("class", "hidden")}, 3000);
+    })
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
